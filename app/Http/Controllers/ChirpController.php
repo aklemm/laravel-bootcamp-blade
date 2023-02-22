2d2f22b70a3ec682db089c584020aead0cc3d03a
Aaron Klemm <klemm.aaron@gmail.com>
2023-02-22 10:35:17 -0500

    Implement saving and publishing Chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -15,7 +15,9 @@ class ChirpController extends Controller
      */
     public function index(): View
     {
-        return view('chirps.index');
+        return view('chirps.index', [
+        'chirps' => Chirp::with('user')->latest()->get()
+    ]);
     }
 
     /**
@@ -31,7 +33,13 @@ class ChirpController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
